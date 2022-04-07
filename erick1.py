5c4ef32e13c406c841199061d07f58f67d870eb4
ErickWDaniel <67169390+ErickWDaniel@users.noreply.github.com>
2022-04-07 23:45:50 +0300

    Initial Commit

---
+++ erick1.py
@@ -0,0 +1,42 @@
+from tkinter import *
+
+from gtts import gTTS
+from playsound import playsound
+
+root = Tk()
+root.geometry("500x500")
+root.configure(bg='ghost white')
+root.title("Sikiliza maneno")
+
+Label(root, text="ANDIKA HAPA", font="arial 20 bold", bg='white smoke').pack()
+Label(text="Erick App", font='arial 15 bold', bg='white smoke', width='20').pack(side='bottom')
+
+Msg = StringVar()
+Label(root, text="Enter Text", font='arial 15 bold', bg='white smoke').place(x=20, y=60)
+
+entry_field = Entry(root, textvariable=Msg, width='70')
+entry_field.place(x=20, y=100)
+
+
+def Text_to_speech():
+    Message = entry_field.get()
+    speech = gTTS(text=Message)
+    speech.save('erickvoice.mp3')
+    playsound('erickvoice.mp3')
+
+
+def Exit():
+    root.destroy()
+
+
+def Reset():
+    Msg.set("")
+
+
+Button(root, text="PLAY", font='arial 15 bold', command=Text_to_speech, width='4', bg='Pink').place(x=25, y=140)
+
+Button(root, font='arial 15 bold', text='EXIT', width='4', command=Exit, bg='Cyan').place(x=100, y=140)
+
+Button(root, font='arial 15 bold', text='RESET', width='6', command=Reset, bg='Orange').place(x=175, y=140)
+
+root.mainloop()
---
+++ venv/gttsE.py
@@ -0,0 +1,16 @@
+#pip3 install pyttsx3
+#apt-get install alsa-utils
+
+# tts = gTTS(text='what to say', lang='en')
+# tts.save('/path/to/file.mp3')
+#
+#
+# import pyttsx3, time
+# engine = pyttsx3.init()
+# engine.say("Hi, I am text to speach")
+# engine.runAndWait()
+
+import pyttsx3;
+engine = pyttsx3.init();
+engine.say("I will Fuck this text");
+engine.runAndWait()
